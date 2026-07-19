52a59241a457cad042c72d50301c081af832182f
Adem-bit <ademgallopeni3@gmail.com>
2026-07-19 11:29:43 +0200

    Refactor start logic with guard clauses

--- car_game.py
+++ car_game.py
@@ -1,13 +1,22 @@
 class Car:
-    def __init__(self, color, brand, horsepower):
+    def __init__(self, color, brand, fuel_level):
         self.color = color
         self.brand = brand
-        self.horsepower = horsepower
+        self.fuel_level = fuel_level
         self.is_running = False
 
     def start(self):
-        self.is_running = True
-        print(f"The {self.color} {self.brand} has started!")
+        if self.is_running:
+            print(f"The {self.color} {self.brand} is already running!")
+            return
+
+        if self.fuel_level < 5:
+            print("Out of gas!")
+            self.is_running = False
+        elif not self.is_running and self.fuel_level >= 5:
+            self.is_running = True
+            print(f"The {self.color} {self.brand} has started!")
+            self.fuel_level -= 5
 
     def stop(self):
         if self.is_running:
@@ -16,7 +25,14 @@ class Car:
         else:
             print(f"The {self.color} {self.brand} is already stopped!")
 
+    def refuel(self):
+        self.fuel_level = 100
+        print(f"The {self.color} {self.brand} is refueled!")
 
-my_red_car = Car("red", "bmw")
+
+my_red_car = Car("red", "bmw", 5)
 my_red_car.start()
 my_red_car.stop()
+my_red_car.start()
+my_red_car.refuel()
+my_red_car.start()
