485a00aa89af1e80f6f8c4a3e04983dfaf9b089a
Adem-bit <ademgallopeni3@gmail.com>
2026-07-20 23:15:52 +0200

    Feature: Added drive system with fuel cost and odometer

--- car_game.py
+++ car_game.py
@@ -4,6 +4,7 @@ class Car:
         self.brand = brand
         self.fuel_level = fuel_level
         self.is_running = False
+        self.mileage = 0
 
     def start(self):
         if self.is_running:
@@ -29,10 +30,38 @@ class Car:
         self.fuel_level = 100
         print(f"The {self.color} {self.brand} is refueled!")
 
+    def drive(self, distance):
+        cost = distance / 10
+        if not self.is_running:
+            print(
+                "Your car is not turned on, you can't drive!")
+            return
 
-my_red_car = Car("red", "bmw", 5)
-my_red_car.start()
-my_red_car.stop()
-my_red_car.start()
-my_red_car.refuel()
-my_red_car.start()
+        if self.fuel_level < cost:
+            print("Not enough gas for this trip!")
+            return
+        self.fuel_level -= cost
+        self.mileage += distance
+        print(f"You went {distance} miles!")
+
+    def show_stats(self):
+        print(f"\n--- {self.brand} Dashboard ---")
+        print(f"Status: {'Running' if self.is_running else 'Off'}")
+        print(f"Fuel: {self.fuel_level}%")
+        print(f"Mileage: {self.mileage} miles")
+        print("-------------------\n")
+
+
+my_car = Car("blue", "Ford", 150)
+my_car.fuel_level = 20
+
+my_car.drive(50)
+
+my_car.start()
+
+my_car.drive(50)
+
+print(f"Total mileage: {my_car.mileage}")
+print(f"Fuel left: {my_car.fuel_level}")
+
+my_car.drive(200)
